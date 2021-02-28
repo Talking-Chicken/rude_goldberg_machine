dc9cedbc6a07358f5f012ad46c143f67da49606a
talking chicken <sj3026@nyu.edu>
2021-02-28 20:23:02 +0800

    putting basic physics code in

---
+++ Assets/script/ballBehaviour.cs
@@ -0,0 +1,49 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ballBehaviour : MonoBehaviour
+{
+
+    SpriteRenderer myRender;
+    public Color floorColor;
+    public Color gateColor;
+
+    Rigidbody2D myBody;
+
+    public float power;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        myRender = GetComponent<SpriteRenderer>();
+        myBody = GetComponent<Rigidbody2D>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            Debug.Log("Space Down");
+            myBody.AddForce((new Vector3(1, 0, 0) + new Vector3(0, 10, 0)) * power);
+        }
+    }
+
+    private void OnCollisionEnter2D(Collision2D other)
+    {
+        if (other.gameObject.name == "floor")
+        {
+            myRender.color = floorColor;
+            Debug.Log("HIT");
+        }
+    }
+
+    void OnTriggerEnter2D(Collider2D other)
+    {
+        if (other.gameObject.name == "gate")
+        {
+            myRender.color = gateColor;
+        }
+    }
+}
---
+++ Assets/script/cameraMovement.cs
@@ -0,0 +1,56 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class cameraMovement : MonoBehaviour
+{
+    public Transform followTransform;
+    public BoxCollider2D worldBounds;
+
+    float xMax;
+    float xMin;
+    float yMax;
+    float yMin;
+
+    float camX;
+    float camY;
+
+    float camRatio;
+    float camSize;
+
+    Camera mainCamera;
+
+    Vector3 smoothPos;
+
+    public float smoothRate;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        xMin = worldBounds.bounds.min.x;
+        xMax = worldBounds.bounds.max.x;
+        yMin = worldBounds.bounds.min.y;
+        yMax = worldBounds.bounds.max.y;
+
+        mainCamera = gameObject.GetComponent<Camera>();
+
+        camSize = mainCamera.orthographicSize;
+        camRatio = (xMax + camSize) / 8.0f;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    private void FixedUpdate()
+    {
+        camY = Mathf.Clamp(followTransform.position.y, yMin + camSize, yMax - camSize);
+        camX = Mathf.Clamp(followTransform.position.x, xMin + camRatio, xMax - camRatio);
+
+        smoothPos = Vector3.Lerp(gameObject.transform.position, new Vector3(camX, camY, gameObject.transform.position.z), smoothRate);
+
+        gameObject.transform.position = smoothPos;
+    }
+}
